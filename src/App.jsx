a150288b1c9fcf319c864ef809f3789936e691bd
ULLASK18 <1jt22cs176@jyothyit.ac.in>
2024-10-26 23:25:09 +0530

    Fixed routing for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,24 @@ import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Signup from './Signup';        // Load Signup component here
+import Signup from './Signup';
 import Login from './Login';
 import SurveyHome from './components/SurveyHome';
 import Result from './components/result';
 import Home from './pages/Home';
+import Register from './pages/Register';
 
 function App() {
   return (
     <div className="bg-background">
+      {/* Set the basename to match your GitHub Pages repository */}
       <BrowserRouter basename="/online-survey-system">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/register" element={<Signup />} />  {/* Use Signup here */}
+          <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
           <Route path="/home" element={<SurveyHome />} />
           <Route path="/result" element={<Result />} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </div>
